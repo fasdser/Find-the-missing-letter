ce3d3f6be80e47a593f13956572cfc68251d0f46
fasdser <107477645+fasdser@users.noreply.github.com>
2022-10-27 22:54:09 +0300

    Add project files.

---
+++ Find the missing letter/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_the_missing_letter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindMissingLetter(new[] { 'O', 'Q', 'R', 'S' }));
+            Console.ReadLine();
+        }
+
+       
+            public static char FindMissingLetter(char[] array)
+            {
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i + 1] - array[i] > 1)
+                    {
+                        return (char)(array[i] + 1);
+                    }
+                }
+
+                return ' ';
+            }
+
+        public static char FindMissingLetter1(char[] array) => (char)(array.Where((c, i) => array[i + 1] - c != 1).First() + 1);
+
+    }
+
+}
